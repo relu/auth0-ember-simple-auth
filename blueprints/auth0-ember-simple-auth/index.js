11360e13510d095552bda602ac5da03877c5278e
Aurel Canciu <aurelcanciu@cloud.upwork.com>
2017-03-10 13:44:18 +0200

    Update auth0-lock version

--- blueprints/auth0-ember-simple-auth/index.js
+++ blueprints/auth0-ember-simple-auth/index.js
@@ -4,7 +4,7 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'auth0-lock',             target: '^7.10.2'  },
+      { name: 'auth0-lock',             target: '^10.12.3'  },
       { name: 'jsrsasign',              target: '^5.0.1'  }
     ]);
   }
